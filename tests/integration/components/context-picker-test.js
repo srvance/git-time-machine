05fa46ceac6e5ef31e5a4495a2ae74e27fec6737
Stephen Vance <steve@vance.com>
2016-12-26 13:00:52 -0500

    Componentize the context picker

---
+++ tests/integration/components/context-picker-test.js
@@ -0,0 +1,25 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('context-picker', 'Integration | Component | context picker', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{context-picker}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#context-picker}}
+      template block text
+    {{/context-picker}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
