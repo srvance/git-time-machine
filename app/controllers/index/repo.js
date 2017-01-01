211fbe09924d0c65271cef9773899912439bba57
Stephen Vance <steve@vance.com>
2017-01-01 10:20:28 -0500

    Straggler deletion.

--- app/controllers/index/repo.js
+++
@@ -1,6 +0,0 @@
-import Ember from 'ember';
-import ContextActionsMixin from 'git-time-machine/mixins/context-actions';
-
-export default Ember.Controller.extend(ContextActionsMixin, {
-  context: Ember.inject.service()
-});
